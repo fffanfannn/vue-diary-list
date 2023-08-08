e209872177d4713a6bcb4cc7db56170b13ac1f91
fffanfannn <2295467@edu.vaniercollege.qc.ca>
2023-08-08 02:15:15 -0400

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -46,3 +46,11 @@ describe("DiaryList.vue", () => {
   it("if 'Next' button is clicked, it will move to previous three items", () => {});
 
 });
+
+describe("App.vue", () => {
+  it("when there is a new object, this object will be added to the original array", () => {
+    const wrapper = shallowMount(App);
+  });
+
+  it("when there is delete id, the object of this id will be removed from the original array", () => {});
+});
